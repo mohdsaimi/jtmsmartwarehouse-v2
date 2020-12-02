06b4b68c161d862d9e48249e746c02b9e03feeea
Nurzaly Abd Rahman <nrzaly@gmail.com>
2020-12-02 14:52:24 +0800

    edit dashboard

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -10,6 +10,7 @@
                     <strong>@lang('strings.backend.dashboard.welcome') {{ $logged_in_user->name }}!</strong>
                 </div><!--card-header-->
                 <div class="card-body">
+                    test
                     {!! __('strings.backend.welcome') !!}
                 </div><!--card-body-->
             </div><!--card-->
